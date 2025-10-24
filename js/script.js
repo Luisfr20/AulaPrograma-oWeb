98a6123803ed34eaa382f940b94cbc25c9f8bafb
Ribeiros <luisfribeiro02@gmail.com>
2025-10-24 12:18:41 -0300

    adicionando responsivdade e arquivos js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const menuToggle = document.getElementById("menu-toggle");
+const menuLinks = document.getElementById("menu-links");
+
+menuToggle.addEventListener("click", () => {
+	menuLinks.classList.toggle("ativo");
+});
+
