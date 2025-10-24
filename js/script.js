1e70f1fc7397359bfa2502ab57751f7d97112965
Luisfr20 <luisfribeiro02@gmail.com>
2025-10-24 09:29:43 -0300

    Add files via upload
    responsividade e menu hamburguer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-script.js
+
+const menuToggle = document.getElementById("menu-toggle");
+const menuLinks = document.getElementById("menu-links");
+
+menuToggle.addEventListener("click", () => {
+	menuLinks.classList.toggle("ativo");
+});
+
